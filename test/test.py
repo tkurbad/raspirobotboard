3948f8ddbdf770adad7827a7804ea6f724521611
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2020-10-06 17:45:22 +0200

    Archived

--- test/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2
-
-from torbot.torbot import TorBot
-
-bot = TorBot()
-
-#bot.test()
-bot.test(motors = False, camera = False)
--- test/test_concurrent.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python2
-
-from torbot.torbot import TorBotThreadController
-
-bot = TorBotThreadController()
-
-bot.test()
